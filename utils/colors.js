acb428d95f36e0319416a0526441cdffc7794ccd
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-03-30 22:01:52 -0300

    Color class

---
+++ utils/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    purple: '#292477',
+    gray: '#757575',
+    white: '#fff',
+    red: '#b71845',
+    orange: '#f26f28',
+    blue: '#4e4cb8',
+    lightPurp: '#7c53c3',
+    pink: '#b93fb3'
+}
+
+export default colors
