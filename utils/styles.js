d3b540eea9adfa1b3d94fe8484d46dece766cb80
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-03-30 22:02:08 -0300

    Custom styles

---
+++ utils/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+export const activityIndicatorStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
