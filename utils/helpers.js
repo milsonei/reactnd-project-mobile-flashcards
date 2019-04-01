6c879c7d0d1bfe3df61e17cd51ad43710f6fa4db
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-01 17:43:37 -0300

    Implementing routine to load initial application data

--- utils/helpers.js
+++ utils/helpers.js
@@ -194,3 +194,30 @@ export function showWarningToast(message, duration = 5000) {
     duration
   })
 }
+
+export function getInitialData() {
+  return {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    }
+  }
+}
