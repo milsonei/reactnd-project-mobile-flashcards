79f48944844f5bf3f74cde4e8772c5e94f39ca68
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-01 17:44:41 -0300

    Inclusion of new features for the main container

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,32 +1,85 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux'
-import { Container, Header, Left, Right, Body, Content, Button, Icon, Title } from 'native-base'
+import {
+    Container,
+    Header,
+    Left,
+    Right,
+    Body,
+    Content,
+    Button,
+    Icon,
+    Title,
+    Badge,
+    Footer,
+    FooterTab,
+    Text
+} from 'native-base'
 import TopStatusBar  from './TopStatusBar'
 import colors from '../utils/colors'
-export default class MainContainer extends Component{  
+
+class MainContainer extends Component{
+    currentTab = () => this.props.currentScene.toLowerCase()
+    goHome = () => {       
+        if (this.currentTab() === 'home') return
+        Actions.pop()
+    }
+    goNewDeck = () => {
+        if (this.currentTab() === 'newdeck') return
+        Actions.NewDeck()
+    }
     render() {
-        const leftButton = this.props.leftButton ||
-                         (<Button transparent onPress={() => Actions.pop()}>                    
-                            <Icon name='arrow-back' />
-            </Button>)
+        const { footer, header, currentScene, title } = this.props
+        const currentTab = currentScene.toLowerCase()
         const style = this.props.style || {}
-        const rightButton = this.props.rightButton
+        
         return (
             <Container>
-                <TopStatusBar backgroundColor={colors.purple} barStyle='light-content'/>
-                <Header>
-                    <Left>
-                       {leftButton}
-                    </Left>
-                    <Body>
-                        <Title>{this.props.title}</Title>
-                    </Body> 
-                    {rightButton && (<Right>{rightButton}</Right>)}
-                </Header>
-                <Content style={{ padding: 20,...style }}>  
+                <TopStatusBar backgroundColor={colors.purple} barStyle='light-content' />
+                {header
+                    && (<Header>
+                        <Left>
+                            <Button transparent onPress={() => Actions.pop()}>                    
+                            <Icon name='arrow-back' />           
+                        </Button>
+                        </Left>
+                        <Body>
+                            <Title>{title}</Title>
+                        </Body>         
+                    </Header>)}
+                <Content contentContainerStyle={{flex: 1}} style={{ padding: 20,...style }}>  
                     {this.props.children}
                 </Content>
+                {footer && (<Footer>
+                    <FooterTab>
+                        <Button active={currentTab === 'home'}
+                                badge
+                                vertical
+                                onPress={() => this.goHome()}>
+                            <Badge><Text>1</Text></Badge>
+                            <Icon name="apps" />
+                            <Text>Decks</Text>
+                        </Button>
+                        <Button
+                            active={currentTab === 'newdeck'}
+                            vertical
+                            onPress={() => this.goNewDeck()}>
+                            <Icon name="add-circle"/>
+                            <Text>New Deck</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>)}
           </Container>
         )
     }
 }
+
+const mapStateToProps = ({ flux }) => {
+    const { currentScene } = flux  
+    return {
+      currentScene
+    }
+};
+  
+export default connect(mapStateToProps)(MainContainer)
