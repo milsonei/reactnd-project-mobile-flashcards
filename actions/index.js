a7e39b524c7a7d011abfcd5bcb9bc5e431934f77
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-03 20:06:20 -0300

    Implementation of the method responsible for marking the quiz as done

--- actions/index.js
+++ actions/index.js
@@ -1,10 +1,13 @@
 import storage from '../utils/MyStorage';
 import { setError } from './error';
 import { setSuccess } from './success';
+import { getCurrentDate } from '../utils/helpers';
+import { restartQuiz } from './quiz';
 export const RECEIVE_DECKS = 'RECEIVE_DECKS'
 export const ADD_DECK = 'ADD_DECK'
 export const ADD_CARD = 'ADD_CARD'
 export const RECEIVE_CARDS = 'RECEIVE_CARDS'
+export const SET_COMPLETED_QUIZ = 'SET_COMPLETED_QUIZ'
 /**
  * Receive a list of decks in normalized format
  * @param {any} decks list of decks as object
@@ -27,7 +30,32 @@ export function addDeck(title){
         payload: {
             deck: {
                 title,
+                lastQuiz: {
+                    date: '',
+                    correct: 0,
+                    incorrect: 0
+                },
                 questions: []
+            }
+        }
+    }
+}
+
+/**
+ * Set completed quiz for a specific deck
+ * @param {string} deck deck title
+ * @param {number} correct number of correct questions
+ * @param {number} incorrect number of incorrect questions
+ */
+export function setCompletedQuiz(deck, correct, incorrect){
+    return {
+        type: SET_COMPLETED_QUIZ,
+        payload: {
+            quiz: {
+                deck,
+                date: getCurrentDate(),
+                correct,
+                incorrect
             }
         }
     }
@@ -60,6 +88,7 @@ export async function handleAddDeck(title, dispatch) {
         } else {
             return storage.saveDeckTitle(title).then(() => {        
                 dispatch(addDeck(title))
+                dispatch(restartQuiz())
                 dispatch(setSuccess())
             }).catch(() => {
                 dispatch(setError('Error', 'There was an error inserting the deck. Try again.'))
@@ -89,8 +118,18 @@ export async function handleAddCardToDeck(title, card, dispatch) {
     try {
         await storage.addCardToDeck(title, card)
         dispatch(addCardToDeck(title, card))
+        dispatch(restartQuiz())
         dispatch(setSuccess())
     } catch (ex) {
         dispatch(setError('Error', 'Error on add cards. Try again.'))
+    }
+}
+
+export async function handleSetCompletedQuiz(deck, correct, incorrect, dispatch) {   
+    try {
+        await storage.setLastCompletedQuiz(deck, correct, incorrect)
+        dispatch(setCompletedQuiz(deck, correct, incorrect))
+    } catch (ex) {
+        dispatch(setError('Error', 'Error on setting completed quiz. Try again.'))
     }
 }
