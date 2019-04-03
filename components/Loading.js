2d7d3809c20b5752b7b4e3eeaa5f8afe3d714ca3
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-03 20:07:19 -0300

    Documentation

--- components/Loading.js
+++ components/Loading.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { View, ActivityIndicator } from 'react-native'
 import { activityIndicatorStyles } from '../utils/styles'
+/**
+ * Component that renders an activity indicator while the screen data is loaded
+ */
 export default function Loading() {
     return (
         <View style={[activityIndicatorStyles.container, activityIndicatorStyles.horizontal]}>
