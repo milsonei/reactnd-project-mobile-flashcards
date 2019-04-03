c83b84610ac1b036c54230535c459505714724f6
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-03 20:03:34 -0300

    Remove current param

--- actions/quiz.js
+++ actions/quiz.js
@@ -1,31 +1,27 @@
+import storage from "../utils/MyStorage";
+
 export const ADD_CORRECT = "ADD_CORRECT"
 export const ADD_INCORRECT = "ADD_INCORRECT"
 export const RESTART_QUIZ = "RESTART_QUIZ"
 /**
  * Action to indicate that answer is correct
  * @param {string} deck Name of deck
- * @param {number} current Current index of question
  */
-export function addCorrect(deck, current){    
+export function addCorrect(deck){    
     return {
         type: ADD_CORRECT,
-        payload: {
-                    deck,
-                    current}
+        payload: { deck }
     }
 }
 
 /**
  * Action to indicate that answer is incorrect
  * @param {string} deck Name of deck
- * @param {number} current Current index of question
  */
-export function addIncorrect(deck, current){    
+export function addIncorrect(deck){    
     return {
         type: ADD_INCORRECT,
-        payload: {
-            deck,
-            current}
+        payload: { deck }
     }
 }
 
