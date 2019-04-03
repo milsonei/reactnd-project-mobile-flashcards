ca6fc01e9432a41bfa23f76d1af27a06f593e0e0
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-03 20:07:07 -0300

    Add transition configuration for navigation

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -5,18 +5,19 @@ import NewDeckScreen from '../screens/NewDeckScreen'
 import DeckViewScreen from '../screens/DeckViewScreen'
 import NewCardScreen from '../screens/NewCardScreen'
 import QuizViewScreen from '../screens/QuizViewScreen'
+import transitionConfig from './TransitionConfig'
 /**
  * Pass Scene to create your app navigator and the Props the Router should receive. 
  * It is alternative router creation method mostly used for Redux integration
  */
 const AppNavigator = Actions.create(
-    <Scene key="root"  hideNavBar ={true}>
-        <Scene key="Home" component={HomeScreen}/>
+    <Scene key="root" hideNavBar ={true} transitionConfig={transitionConfig}>
+        <Scene key="Home" component={HomeScreen} initial/>
         <Scene key="NewDeck" component={NewDeckScreen}/>
         <Scene key="DeckView" component={DeckViewScreen}/>
         <Scene key="NewCard" component={NewCardScreen}/>
         <Scene key="QuizView" component={QuizViewScreen}/>
     </Scene>
 )
-    
+
 export default AppNavigator
