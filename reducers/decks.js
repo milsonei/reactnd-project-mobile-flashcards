6883d8b282ee3a4d65c1115b1692e7b709a37691
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:54:40 -0300

    Set complete quiz implemented

--- reducers/decks.js
+++ reducers/decks.js
@@ -1,11 +1,30 @@
 import {
     RECEIVE_DECKS,
     ADD_DECK,
-    ADD_CARD
+    ADD_CARD,
+    SET_COMPLETED_QUIZ
 } from '../actions'
-
+/**
+ * Reducer responsible for the management of the decks and cards
+ * @param {any} state 
+ * @param {any} action 
+ */
 const decks = (state = {}, { type, payload }) => {
     switch (type) {
+        case SET_COMPLETED_QUIZ:
+        const { quiz } = payload
+        return {
+            ...state,
+            [quiz.deck]: {
+                ...state[quiz.deck],
+                lastQuiz: {
+                    date: quiz.date,
+                    correct: quiz.correct,
+                    incorrect: quiz.incorrect
+                }
+                
+            }
+        }
         case RECEIVE_DECKS:
             return {
                 ...state,
