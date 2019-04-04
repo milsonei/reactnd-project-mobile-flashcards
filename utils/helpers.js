40a0be5bdacedc01114e00d8fd2e525b375dd9ba
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:47:54 -0300

    Refactoring

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,121 +1,12 @@
-import { Alert, StyleSheet, AsyncStorage } from 'react-native'
+import { Alert } from 'react-native'
 import { Toast } from 'native-base';
-import { Notifications, Permissions, Constants } from 'expo'
-const NOTIFICATION_KEY = 'MobileFlashcards:notifications'
-export function isBetween (num, x, y) {
-    if (num >= x && num <= y) {
-      return true
-    }
-  
-    return false
-  }
-  
-  export function calculateDirection (heading) {
-    let direction = ''
-    
-    if (isBetween(heading, 0, 22.5)) {
-      direction = 'North'
-    } else if (isBetween(heading, 22.5, 67.5)) {
-      direction = 'North East'
-    } else if (isBetween(heading, 67.5, 112.5)) {
-      direction = 'East'
-    } else if (isBetween(heading, 112.5, 157.5)) {
-      direction = 'South East'
-    } else if (isBetween(heading, 157.5, 202.5)) {
-      direction = 'South'
-    } else if (isBetween(heading, 202.5, 247.5)) {
-      direction = 'South West'
-    } else if (isBetween(heading, 247.5, 292.5)) {
-      direction = 'West'
-    } else if (isBetween(heading, 292.5, 337.5)) {
-      direction = 'North West'
-    } else if (isBetween(heading, 337.5, 360)) {
-      direction = 'North'
-    } else {
-      direction = 'Calculating'
-    }
-  
-    return direction
-  }
-  
+
   export function timeToString (time = Date.now()) {
     const date = new Date(time)
     const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
     return todayUTC.toISOString().split('T')[0]
   }
 
-  const styles = StyleSheet.create({
-    iconContainer: {
-      padding: 5,
-      borderRadius: 8,
-      width: 50,
-      height:50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginRight:20
-    }
-  })
-
-
-export function clearLocalNotification(){
-  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(Notifications.cancelAllScheduledNotificationsAsync)
-}
-  
-function createNotification(){
-  const localnotification = {
-    title: "Log your stats",
-    body: "👋 don't forget to log your stats for today",
-    ios: {
-      sound: true
-    },
-    android: {
-      sound: true,
-      priority: 'high',
-      stick: false,
-      vibrate: true
-    },
-    data: {
-
-    },
-  }
-
-  localnotification.data.title = localnotification.title;
-  localnotification.data.body = localnotification.body;
-
-  return localnotification
-}
-export function setLocalNotification() {
-  if (!Constants.isDevice) {
-    console.warn('Device only!')
-    return
-  }
-  AsyncStorage.getItem(NOTIFICATION_KEY).then(JSON.parse).then((data) => {
-    if (data === null) {
-      Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
-        if (status === 'granted') {
-          Notifications.cancelAllScheduledNotificationsAsync().then(() => {
-            let tomorrow = new Date()
-            console.log('Date:', tomorrow)
-            tomorrow.setDate(tomorrow.getDate())
-            tomorrow.setHours(tomorrow.getHours())
-            tomorrow.setMinutes(tomorrow.getMinutes())
-            tomorrow.setSeconds(tomorrow.getSeconds() + 10)
-           
-            Notifications.scheduleLocalNotificationAsync(createNotification(), {
-              time: tomorrow,
-              //repeat: 'minute'
-            }).then((id) => {
-              console.log('id:',id)
-              console.log('Date:', tomorrow)
-            })
-  
-            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
-          })
-        }
-      })
-    }
-  })
-}
 /**
 * Get sort condition to sort function
 * @param {any} firstData First data to compare
@@ -179,7 +70,13 @@ export function showSuccessToast(message, duration = 5000) {
     duration
   })
 }
-
+/**
+ * Get current date in yyyy-mm-dd format
+ */
+export function getCurrentDate() {
+  const date = new Date()
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`
+}
 /**
  * Use NativeBase Toast to display quick warning messages
  * @param {string} message warning message
@@ -199,6 +96,11 @@ export function getInitialData() {
   return {
     React: {
       title: 'React',
+      lastQuiz: {
+        date: '',
+        correct: 0,
+        incorrect: 0,
+      },
       questions: [
         {
           question: 'What is React?',
@@ -212,6 +114,11 @@ export function getInitialData() {
     },
     JavaScript: {
       title: 'JavaScript',
+      lastQuiz: {
+        date: '',
+        correct: 0,
+        incorrect: 0,
+      },
       questions: [
         {
           question: 'What is a closure?',
