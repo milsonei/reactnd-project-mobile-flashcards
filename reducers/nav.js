2b7c7970d9d1b22ac48749feb7207b68e3608d02
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:53:22 -0300

    Refactoring

--- reducers/nav.js
+++ reducers/nav.js
@@ -1,5 +1,10 @@
 import AppNavigator from "../components/AppNavigator";
 const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Home'));
+/**
+ * Reducer responsible for the management of the next scene
+ * @param {any} state 
+ * @param {any} action 
+ */
 const nav = (state = initialState, action) => {
     const nextState = AppNavigator.router.getStateForAction(action, state);
     // Simply return the original `state` if `nextState` is null or undefined.
