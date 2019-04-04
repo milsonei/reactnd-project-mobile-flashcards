2d04c09e174108ad96019e7d694c03507883f1c2
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:53:13 -0300

    Refactoring

--- reducers/quiz.js
+++ reducers/quiz.js
@@ -1,25 +1,39 @@
 import {
     ADD_CORRECT, ADD_INCORRECT, RESTART_QUIZ
 } from '../actions/quiz'
-export default function quiz(state = { deck: '', correct: 0, incorrect: 0 }, { type, payload }) {
+/**
+ * Reducer responsible for the management of the quiz
+ * @param {any} state 
+ * @param {any} action 
+ */
+export default function quiz(state = { deck: '', correct: 0, incorrect: 0, restarted: false }, { type, payload }) {
     switch (type) {        
-        case ADD_CORRECT:                        
+        case ADD_CORRECT:  
+            let correct = state.correct
+            if (state.deck && state.deck !== payload.deck) {
+                correct = 0
+            }    
+            return {                                
+                ...state,
+                deck: payload.deck,
+                correct: correct + 1,
+            }
+        case ADD_INCORRECT:  
+            let incorrect = state.incorrect
+            if (state.deck && state.deck !== payload.deck) {
+                incorrect = 0
+            }        
+            return {
+                ...state,
+                deck: payload.deck,
+                incorrect: incorrect + 1
+            }
+        case RESTART_QUIZ: 
             return {                
-                ...state,
-                deck: payload.deck,
-                correct: state.correct + 1
-            }
-        case ADD_INCORRECT:                        
-            return {
-                ...state,
-                deck: payload.deck,
-                incorrect: state.incorrect + 1
-            }
-        case RESTART_QUIZ:                        
-            return {
                 deck:'',
                 correct: 0,
-                incorrect: 0
+                incorrect: 0,
+                restarted: true
             }           
         default:
             return state
