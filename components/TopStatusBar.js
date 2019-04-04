3207add43431b9ce344e0c8fc1fe8355fce9398d
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:40:18 -0300

    The props were removed and the style setting was defined internally

--- components/TopStatusBar.js
+++ components/TopStatusBar.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { Constants } from "expo";
-function TopStatusBar({ backgroundColor, ...props }) {
+import colors from '../utils/colors';
+/**
+ * Component that renders top status bar
+ * @param {any} props
+ */
+function TopStatusBar() {  
   return (
-    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    <View style={{ backgroundColor:colors.purple, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={colors.purple} barStyle='light-content'/>
     </View>
   )
 }
