476875085c0c5e301ea0e48437a63fa3bef70eec
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:57:21 -0300

    Container header component

---
+++ components/ContainerHeader.js
@@ -0,0 +1,36 @@
+
+                    
+import React from 'react'
+import {
+    Body,
+    Header,
+    Left,
+    Button,
+    Icon,
+    Title
+} from 'native-base'
+import PropTypes from 'prop-types'
+/**
+ * Component that renders the header inside container from Native Base
+ * @param {any} props 
+ */
+const ContainerHeader = (props) => {
+    const { title, onGoBack } = props 
+    return (<Header>
+                <Left>
+                    <Button transparent onPress={() => onGoBack()}>                    
+                    <Icon name='arrow-back' />           
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>{title}</Title>
+                </Body>
+            </Header>)
+}
+
+ContainerHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    onGoBack: PropTypes.func.isRequired,
+}
+
+export default ContainerHeader
