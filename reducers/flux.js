31abaa6b4ea5efd6bfff0bcab2fc14c6899ffdd5
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:41:07 -0300

    logging only in the development environment

--- reducers/flux.js
+++ reducers/flux.js
@@ -1,11 +1,18 @@
 import { ActionConst } from 'react-native-router-flux'
 
 const INITIAL_STATE = { data: null, currentScene: 'home' }
-
+const log = process.env.NODE_ENV === 'development'
+/**
+ * Reducer responsible for the management of the update scenes
+ * @param {any} state 
+ * @param {any} action 
+ */
 const flux = (state = INITIAL_STATE, { type, payload, routeName }) => {
   switch (type) {
     case ActionConst.FOCUS:
-      console.log('FOCUS event fired with scene parameter: ', routeName);
+      if (log) {
+        console.log('FOCUS event fired with scene parameter: ', routeName);
+      }
       return { ...state, currentScene: routeName };
     case 'data':
       return { ...state, data: payload };
