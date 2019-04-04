f857f5db8fa567f119f72122e0eeec6c3b9e84d2
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-04-04 08:53:33 -0300

    Refactoring

--- reducers/error.js
+++ reducers/error.js
@@ -1,6 +1,11 @@
 import {
     SET_ERROR, CLEAR_ERROR
 } from '../actions/error'
+/**
+ * Reducer responsible for the management of the errors
+ * @param {any} state 
+ * @param {any} action 
+ */
 export default function error(state = null, { type, payload }) {
     switch (type) {        
         case SET_ERROR:                        
